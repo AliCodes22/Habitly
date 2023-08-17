c6fbed64e83517e03f8000d103448df10b5b547b
Ali Ahmed <alitine94@gmail.com>
2023-08-17 03:20:44 -0400

    Landing page done

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -1,14 +1,81 @@
 import styled from "styled-components";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <StyledBtn>Styled button</StyledBtn>
-    </div>
+    <Wrapper>
+      <nav></nav>
+      <div className="container page">
+        <div className="info">
+          <h1>Habitly</h1>
+          <p>
+            A platform to systematically build positive habits and eliminate
+            disempowering ones
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login
+          </Link>
+        </div>
+        <img src={main} alt="" className="img main-img" />
+      </div>
+    </Wrapper>
   );
 };
 
+const Wrapper = styled.section`
+  nav {
+    width: 90vw;
+    max-width: 1120px;
+    margin: 0 auto;
+    height: 6rem;
+    display: flex;
+    align-items: center;
+  }
+
+  .container {
+    width: 90vw;
+    max-width: 1120px;
+    margin: 0 auto;
+  }
+  .page {
+    min-height: calc(100vh - 90vw);
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    margin-bottom: 1.5rem;
+  }
+  p {
+    line-height: 2;
+    color: #64748b;
+    margin-bottom: 1.5rem;
+    max-width: 35em;
+  }
+  .register-link {
+    margin-right: 1rem;
+  }
+  .main-img {
+    display: none;
+  }
+  .btn {
+    padding: 0.75rem 1rem;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 400px;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
 const StyledBtn = styled.button`
   font-size: 1.5rem;
   background: red;
