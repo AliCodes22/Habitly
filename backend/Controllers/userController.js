c467a54e5e7e4c00aaf3ce32349c06a26b4a02c8
Ali Ahmed <alitine94@gmail.com>
2023-08-20 03:30:58 -0400

    register under progress

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -24,11 +24,25 @@ const registerUser = async (req, res) => {
 
   try {
     const db = await client.db("habitly");
+    console.log(req.body);
+
+    if (!email || !password || !email) {
+      res.status(404).json({
+        message: "invalid fields",
+      });
+    }
 
     const { name, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = { name, password: hashedPassword, email, userId: uuidv4() };
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = {
+      name,
+      password: hashedPassword,
+      email,
+      userId: uuidv4(),
+    };
     await db.collection("users").insertOne(user);
 
     res.status(201).json({
