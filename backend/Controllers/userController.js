7476ae68fcf0a6c93f26e31b7ce581b59c246851
Ali Ahmed <alitine94@gmail.com>
2023-08-20 02:07:46 -0400

    register functionality good to go

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,11 +1,16 @@
 const { MongoClient } = require("mongodb");
+const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require("uuid");
+
 require("dotenv").config();
 const { MONGO_URI } = process.env;
+
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-const { v4: uuidv4 } = require("uuid");
+
+const client = new MongoClient(MONGO_URI, options);
 
 const loginUser = (req, res) => {
   res.status(201).json({
@@ -14,11 +19,28 @@ const loginUser = (req, res) => {
   });
 };
 
-const registerUser = (req, res) => {
-  res.status(201).json({
-    message: "register user",
-    status: 201,
-  });
+const registerUser = async (req, res) => {
+  await client.connect();
+
+  try {
+    const db = await client.db("habitly");
+
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = { name, password: hashedPassword, email, userId: uuidv4() };
+    await db.collection("users").insertOne(user);
+
+    res.status(201).json({
+      status: 201,
+      data: user,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 404,
+      message: err.message,
+    });
+  }
 };
 
 module.exports = {
